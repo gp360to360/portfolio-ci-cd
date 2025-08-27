9013c65bbb08650f53be27b84075771d0d2102e1
RahulSingh <chouhanrahul1999@gmail.com>
2025-08-27 23:31:23 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: [
-      'ec2-54-81-144-152.compute-1.amazonaws.com',
-      'localhost',
-      '127.0.0.1'
-    ]
-  }
+  plugins: [tailwindcss(), react()],
 })
