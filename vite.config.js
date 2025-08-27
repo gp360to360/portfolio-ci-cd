9cbc4fd4b02e4f34c26d7ab0e02ac1be0ff103cf
RahulSingh <chouhanrahul1999@gmail.com>
2025-08-28 00:05:48 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'ec2-54-196-176-226.compute-1.amazonaws.com',
+      'ec2-54-81-144-152.compute-1.amazonaws.com',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
